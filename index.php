1fefbd5aff5c3a635836a90bf374bf346559f1ce
Chaybe <chaybe_lucas@hotmail.com>
2022-01-19 17:45:58 -0400

    base

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Chaybe's Place</title>
+        <link rel="stylesheet" type="text/css" href="src/css/principal.css" />
+        <link rel="stylesheet" type="text/css" href="src/css/item.css" />
+    </head>
+    <body>
+        <div class="barra-menu">
+            <div>
+                <div class="titulo">Chaybe</div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="pnl-content">
+            <div class="item">
+                <div class="titulo-item">
+                    FREEWAY
+                </div>
+                <div>
+                    Jogue na mesma máquina com um amigo! FREEWAY é um jogo onde você é a galinha que a travessa a rua!
+                </div>
+                <div>
+                    <a href="#">
+                        <input type="submit" value="Download" class="btn-download">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
